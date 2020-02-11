f513da60a1081005a8a1a3565c34920179e2c31e
Arren De Manuel <ademanue@ucsd.edu>
2020-02-11 14:09:34 -0800

    started login page

---
+++ Assets/Scripts/TestScript/RotationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RotationTest : MonoBehaviour
+{
+    Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddTorque(10.0f);
+        rb.AddForce(new Vector2(10.0f, 0));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+}
