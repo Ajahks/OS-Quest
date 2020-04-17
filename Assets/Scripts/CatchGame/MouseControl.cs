a890e9c15e47352fea0ef9e729f3b11c9cf6178e
mattlawr <mattblawr@gmail.com>
2020-04-16 20:45:56 -0700

    Update trash texture

---
+++ Assets/Scripts/CatchGame/MouseControl.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * by Matthew Lawrence
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public bool xAxis = true;
+    public bool yAxis = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v3 = Input.mousePosition;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        v3.z = transform.position.z;
+
+        if (!yAxis)
+            v3 = Vector3.Scale(v3, new Vector3(1f, 0f, 1f));
+
+        transform.position = v3;
+    }
+}
