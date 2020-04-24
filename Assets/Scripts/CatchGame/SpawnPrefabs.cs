f1a48420d08700b1a35dc9e7d9df45a6ad4f6011
mattlawr <mattblawr@gmail.com>
2020-04-23 21:00:07 -0700

    Finished catch minigame foundations
    still needs sound, art and polish

--- Assets/Scripts/CatchGame/SpawnPrefabs.cs
+++ Assets/Scripts/CatchGame/SpawnPrefabs.cs
@@ -2,28 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPrefabs : MonoBehaviour
+public class SpawnPrefabs : Minigame
 {
     public float rate = 2f;
+    public float range = 2f;
     float rateTimer;
 
-    public GameObject prefab;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Pickup spawnee;
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         rateTimer += Time.deltaTime;
 
         if(rateTimer >= rate)
         {
-            rateTimer = 0;
-            GameObject go = Instantiate(prefab, transform.position, transform.rotation);
+            Spawn();
         }
+
+        base.Update();
+    }
+
+    void Spawn()
+    {
+        float offset = Random.Range(-range, range);
+
+        // Let the pickup be instantiated
+        Pickup e = Instantiate(spawnee, transform.position + Vector3.right*offset, transform.rotation);
+        e.SetData(this);    // Tell the pickup about the master game object
+
+        rateTimer = 0;
     }
 }
