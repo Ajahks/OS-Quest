5dfb97d6a319bdd00e97760f7d10d25b1cec4fe6
Arren De Manuel <ademanue@ucsd.edu>
2020-05-19 11:16:33 -0700

    started user auth

---
+++ Assets/Scripts/Firebase/FirebaseInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+
+public class FirebaseInit : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+
+
+    public void SignInWithGoogle()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Firebase/UserAuth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAuth : MonoBehaviour
+{
+    [SerializeField] GameObject LoginPanel; // Default panel for loging in
+    [SerializeField] GameObject RegisterPanel; // Default panel for registering
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    // Use these two methods below to swap between the register and the login panel
+    public void EnableRegisterPanel()
+    {
+        RegisterPanel.SetActive(true);
+        LoginPanel.SetActive(false);
+    }
+
+    public void EnableLoginPanel()
+    {
+        LoginPanel.SetActive(true);
+        RegisterPanel.SetActive(false);
+    }
+}
